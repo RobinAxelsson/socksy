972e284f151484ee925c3862081415a6b6336b6f
RobinSalihAxelsson <robin.salihaxelsson.external@fujitsu.com>
2025-10-25 02:17:26 +0200

    reläar hela vägen tillbaka, problemet var att jag inte .FlushAsync() på ReadExaclyAsync

--- src/Program.cs
+++ src/Program.cs
@@ -26,9 +26,12 @@ internal class Program
                 var client = await listener.AcceptTcpClientAsync();
 
                 using var networkStream = client.GetStream();
+                Console.WriteLine("WslRelay: hello_world");
                 var bytes = Encoding.UTF8.GetBytes("hello_world\n");
                 await networkStream.WriteAsync(bytes);
-                var @byte = networkStream.ReadByte();
+                await networkStream.ReadExactlyAsync(bytes); //TODO (robin) why dont we get to this line?
+                var incoming = Encoding.UTF8.GetString(bytes);
+                Console.WriteLine("WslRelay: " + incoming);
             }
         }).ContinueWith(task =>
         {
@@ -54,7 +57,6 @@ internal class Program
             using var wslStream = wslRelayClient.GetStream();
             using var wslReader = new StreamReader(wslStream);
             using var wslWriter = new StreamWriter(wslStream);
-            Console.WriteLine("WinRelay: Connection established to wsl");
 
             var winServiceClient = new HttpClient();
 
@@ -70,7 +72,9 @@ internal class Program
                     var winServiceContent = await response.Content.ReadAsStringAsync();
                     Console.WriteLine("WinRelay: " + winServiceContent);
 
-                    await wslWriter.WriteAsync(winServiceContent);
+                    await wslWriter.WriteAsync(winServiceContent + '\n');
+                    await wslWriter.FlushAsync(); //NOTE (robin): without flushing it does not send theh bytes
+                    await Task.Delay(1000);
                 }
                 await Task.Delay(100);
             }
@@ -90,14 +94,14 @@ internal class Program
         //Example http server on windows that wsl want to reach
         _ = Task.Run(async () =>
         {
-            var winServiceListener = new HttpListener();
-            winServiceListener.Prefixes.Add(WINDOWS_SERVICE_URL);
-            winServiceListener.Start();
+            var httpListener = new HttpListener();
+            httpListener.Prefixes.Add(WINDOWS_SERVICE_URL);
+            httpListener.Start();
             Console.WriteLine("WinService: listening on " + WINDOWS_SERVICE_URL);
 
             while (true)
             {
-                var context = await winServiceListener.GetContextAsync();
+                var context = await httpListener.GetContextAsync();
                 var request = context.Request;
                 var message = request?.Url?.AbsoluteUri.Split('/')[^1] ?? "no message";
                 Console.WriteLine("WinService: " + message);
