fea1e328950193077da252581e35cfe87dcf5953
RobinSalihAxelsson <robin.salihaxelsson.external@fujitsu.com>
2025-10-26 13:10:21 +0100

    moved relays to sepparate threads

--- src/Program.cs
+++ src/Program.cs
@@ -2,94 +2,21 @@
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Unicode;
+using System.Threading.Tasks;
 
 namespace socksy;
 
 internal class Program
 {
+    const int WSL_RELAY_PORT = 8888;
+    const string WINDOWS_SERVICE_URL = "http://localhost:5000/";
+    private static Thread? WslRealayThread;
+    private static Thread? WinRelayThread;
+
     public static async Task Main(string[] args)
     {
-        const int WSL_RELAY_PORT = 8888;
-        const string WINDOWS_SERVICE_URL = "http://localhost:5000/";
-
-        //The WslRelay waits for incoming connection from WindowsRelay
-        _ = Task.Run(async () =>
-        {
-
-            Console.WriteLine("WslRelay: listens on port " + WSL_RELAY_PORT);
-
-            using var listener = new TcpListener(IPAddress.Loopback, WSL_RELAY_PORT);
-            listener.Start();
-
-            while (true)
-            {
-                var client = await listener.AcceptTcpClientAsync();
-
-                using var networkStream = client.GetStream();
-                Console.WriteLine("WslRelay: hello_world");
-                var bytes = Encoding.UTF8.GetBytes("hello_world\n");
-                await networkStream.WriteAsync(bytes);
-                await networkStream.ReadExactlyAsync(bytes); //TODO (robin) why dont we get to this line?
-                var incoming = Encoding.UTF8.GetString(bytes);
-                Console.WriteLine("WslRelay: " + incoming);
-            }
-        }).ContinueWith(task =>
-        {
-            if (task.IsFaulted)
-            {
-                Console.WriteLine("WslRelay: task faulted");
-                Console.WriteLine(task.Exception);
-            }
-            else
-            {
-                Console.WriteLine("WslRelay: task ended");
-            }
-        }).ConfigureAwait(false);
-
-        //The WinRelay initiates the connection to the WslRelay (the listener in this code).
-        _ = Task.Run(async () =>
-        {
-            await Task.Delay(3000);
-
-            using var wslRelayClient = new TcpClient();
-            await wslRelayClient.ConnectAsync(new IPEndPoint(IPAddress.Loopback, WSL_RELAY_PORT));
-
-            using var wslStream = wslRelayClient.GetStream();
-            using var wslReader = new StreamReader(wslStream);
-            using var wslWriter = new StreamWriter(wslStream);
-
-            var winServiceClient = new HttpClient();
-
-            while (true)
-            {
-                var messageToWinService = await wslReader.ReadLineAsync();
-                if (!string.IsNullOrEmpty(messageToWinService))
-                {
-                    Console.WriteLine("WinRelay: " + messageToWinService);
-                    var response = await winServiceClient.GetAsync(WINDOWS_SERVICE_URL + messageToWinService);
-                    response.EnsureSuccessStatusCode();
-                    
-                    var winServiceContent = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine("WinRelay: " + winServiceContent);
-
-                    await wslWriter.WriteAsync(winServiceContent + '\n');
-                    await wslWriter.FlushAsync(); //NOTE (robin): without flushing it does not send theh bytes
-                    await Task.Delay(1000);
-                }
-                await Task.Delay(100);
-            }
-        }).ContinueWith(task =>
-        {
-            if (task.IsFaulted)
-            {
-                Console.WriteLine("WinRelay: task faulted");
-                Console.WriteLine(task.Exception);
-            }
-            else
-            {
-                Console.WriteLine("WinRelay: task ended");
-            }
-        }).ConfigureAwait(false);
+        RunWslRelayThread();
+        RunWinRelayThread();
 
         //Example http server on windows that wsl want to reach
         _ = Task.Run(async () =>
@@ -132,5 +59,95 @@ internal class Program
         {
             await Task.Delay(100);
         }
+    }
+
+    private static void RunWinRelayThread()
+    {
+        void WinRelayStart()
+        {
+            Thread.Sleep(3000);
+
+            using var wslRelayClient = new TcpClient();
+            wslRelayClient.Connect(new IPEndPoint(IPAddress.Loopback, WSL_RELAY_PORT));
+
+            using var wslStream = wslRelayClient.GetStream();
+            using var wslReader = new StreamReader(wslStream);
+            using var wslWriter = new StreamWriter(wslStream);
+
+            var winServiceClient = new HttpClient();
+
+            try
+            {
+                while (true)
+                {
+                    var messageToWinService = wslReader.ReadLine();
+                    if (!string.IsNullOrEmpty(messageToWinService))
+                    {
+                        Console.WriteLine("WinRelay: " + messageToWinService);
+                        var response = winServiceClient.GetAsync(WINDOWS_SERVICE_URL + messageToWinService).GetAwaiter().GetResult();
+                        response.EnsureSuccessStatusCode();
+
+                        var winServiceContent = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                        Console.WriteLine("WinRelay: " + winServiceContent);
+
+                        wslWriter.Write(winServiceContent + '\n');
+                        wslWriter.Flush(); //NOTE (robin): without flushing it does not send theh bytes
+                        Task.Delay(1000);
+                    }
+                    Thread.Sleep(30);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("WinRelay: proc faulted");
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("WinRelay: proc ended");
+        }
+
+        WinRelayThread = new Thread(WinRelayStart);
+        WinRelayThread.Name = "Win Realay thread";
+        WinRelayThread.Start();
+    }
+
+    private static void RunWslRelayThread()
+    {
+        //The WslRelay waits for incoming connection from WindowsRelay
+
+        void WslRelayStart()
+        {
+            Console.WriteLine("WslRelay: listens on port " + WSL_RELAY_PORT);
+
+            using var listener = new TcpListener(IPAddress.Loopback, WSL_RELAY_PORT);
+            listener.Start();
+
+            try
+            {
+                while (true)
+                {
+                    var client = listener.AcceptTcpClient();
+
+                    using var networkStream = client.GetStream();
+                    Console.WriteLine("WslRelay: hello_world");
+                    var bytes = Encoding.UTF8.GetBytes("hello_world\n");
+                    networkStream.Write(bytes);
+                    networkStream.ReadExactly(bytes); //TODO (robin) why dont we get to this line?
+                    var incoming = Encoding.UTF8.GetString(bytes);
+                    Console.WriteLine("WslRelay: " + incoming);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("WslRelay: task faulted");
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine("WslRelay: task ended");
+        }
+
+        WslRealayThread = new Thread(new ThreadStart(WslRelayStart));
+
+        WslRealayThread.Name = "WslRelay Proc";
+        WslRealayThread.Start();
     }
 }
