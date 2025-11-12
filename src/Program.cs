233690c1a80364a591bbfeb9f62fddaec3498d69
RobinSalihAxelsson <robin.salihaxelsson.external@fujitsu.com>
2025-11-12 16:59:02 +0100

    wip

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.IO;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.Unicode;
@@ -6,100 +7,104 @@ using System.Threading.Tasks;
 
 namespace socksy;
 
+internal enum ROLE
+{
+    Connector,
+    Relay
+}
+
 internal class Program
 {
-    const int WSL_RELAY_PORT = 8888;
-    const int e = 8888;
-    const string WINDOWS_SERVICE_URL = "http://localhost:5000/";
-    private static Thread? WslRealayThread;
-    private static Thread? WinRelayThread;
-    //private const int hashCompare = 
-
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        var listener = new TcpListener(IPAddress.Loopback, WSL_RELAY_PORT);
-        listener.Start();
-        var client = listener.AcceptTcpClient();
-        Console.WriteLine("accepted client");
-        using var stream = client.GetStream();
- 
-        var bytes = new byte[8192];
-
-        for (int i = 0; i < bytes.Length; i++)
+        if (args.Length == 0 || (args[0] != "connector" && args[1] != "relay"))
         {
-            var res = stream.ReadByte();
-            if (res == -1)
-            {
-                Console.WriteLine("End of stream");
-                i--;
-                Thread.Sleep(1000);
-                continue;
-            }
-
-            if (i > 3)
-            {
-                int a = bytes[i-3]; //\r
-                int b = bytes[i-2]; //\n
-                int c = bytes[i-1]; //\r
-                int d = bytes[i];   //\n
-
-                if(a == '\r' && b == '\n')
-                {
-                    _ = "";
-                }
-            }
-            
-            bytes[i] = (byte)res;
-            Console.Write((char)res);
+            Console.WriteLine("socksy connector|relay");
+            Environment.Exit(1);
         }
 
+        var state = args[0] == "connector" ? ROLE.Connector : ROLE.Relay;
 
-        //Environment.Exit(0);
+        if (state == ROLE.Connector)
+        {
+            var tcpClient = new TcpClient();
+            tcpClient.Connect(IPAddress.Loopback, 8088);
+            var stream = tcpClient.GetStream();
 
-        //RunWslRelayThread();
-        ////RunWinRelayThread();
+            //var bytes = new byte[8192];
+            int b = stream.ReadByte();
+            var targetClient = new TcpClient();
+            targetClient.Connect(IPAddress.Loopback, 8077); //Python http server on 8077
+            var targetSteam = targetClient.GetStream();
 
-        ////Example http server on windows that wsl want to reach
-        //_ = Task.Run(async () =>
-        //{
-        //    var httpListener = new HttpListener();
-        //    httpListener.Prefixes.Add(WINDOWS_SERVICE_URL);
-        //    httpListener.Start();
-        //    Console.WriteLine("WinService: listening on " + WINDOWS_SERVICE_URL);
+            var writeLoop = Task.Run(() =>
+            {
+                for(int b; (b = stream.ReadByte()) != 0;)
+                {
+                    targetSteam.WriteByte((byte)b);
+                }
+            });
 
-        //    while (true)
-        //    {
-        //        var context = await httpListener.GetContextAsync();
-        //        var request = context.Request;
-        //        var message = request?.Url?.AbsoluteUri.Split('/')[^1] ?? "no message";
-        //        Console.WriteLine("WinService: " + message);
+            var readLoop = Task.Run(() =>
+            {
+                for (int b; (b = targetSteam.ReadByte()) != -1;)
+                {
+                    stream.WriteByte((byte)b);
+                }
+            });
 
-        //        var response = context.Response;
+            var any = await Task.WhenAny(writeLoop, readLoop);
 
-        //        byte[] buffer = Encoding.UTF8.GetBytes(message);
-        //        response.ContentLength64 = buffer.Length;
+            if (any.IsFaulted)
+            {
+                throw any.Exception;
+            }
+        }
 
-        //        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
-        //        response.Close();
-        //    }
-        //})
-        //.ContinueWith(task =>
-        //{
-        //    if (task.IsFaulted)
-        //    {
-        //        Console.WriteLine("WinService: faulted");
-        //        Console.WriteLine(task.Exception);
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("WinService: task ended");
-        //    }
-        //}).ConfigureAwait(false);
+        if (state == ROLE.Relay)
+        {
+            var connectListener = new TcpListener(IPAddress.Loopback, 8088);
+            connectListener.Start();
 
-        //while (true)
-        //{
-        //    await Task.Delay(100);
-        //}
+            var forwardListener = new TcpListener(IPAddress.Loopback, 8066);
+            forwardListener.Start();
+
+            var connectTask = connectListener.AcceptTcpClientAsync();
+            var relayeeTask = forwardListener.AcceptTcpClientAsync();
+
+            var anyFinished = await Task.WhenAny(connectTask, relayeeTask);
+
+            if (anyFinished.IsFaulted)
+            {
+                throw anyFinished.Exception;
+            }
+
+            if (anyFinished == connectTask)
+            {
+                Console.WriteLine("Connected");
+            }
+
+            await Task.WhenAll(connectTask, relayeeTask);
+
+            var connectorClient = await connectTask;
+            var relayee = await relayeeTask;
+
+            var forwardWrite = Task.Run(() =>
+            {
+                for (int b; (b = stream.ReadByte()) != 0;)
+                {
+                    targetSteam.WriteByte((byte)b);
+                }
+            });
+
+            var forwardRead = Task.Run(() =>
+            {
+                for (int b; (b = targetSteam.ReadByte()) != -1;)
+                {
+                    stream.WriteByte((byte)b);
+                }
+            });
+        }
     }
 
     //private static void RunWinRelayThread()
@@ -109,7 +114,7 @@ internal class Program
     //        Thread.Sleep(3000);
 
     //        using var wslRelayClient = new TcpClient();
-    //        wslRelayClient.Connect(new IPEndPoint(IPAddress.Loopback, WSL_RELAY_PORT));
+    //        wslRelayClient.Connect(new IPEndPoint(IPAddress.Loopback, WaiterPort));
 
     //        using var wslStream = wslRelayClient.GetStream();
     //        using var wslReader = new StreamReader(wslStream);
@@ -151,44 +156,44 @@ internal class Program
     //    WinRelayThread.Start();
     //}
 
-    private static void RunWslRelayThread()
-    {
-        //The WslRelay waits for incoming connection from WindowsRelay
+    //private static void RunWslRelayThread()
+    //{
+    //    //The WslRelay waits for incoming connection from WindowsRelay
 
-        void WslRelayStart()
-        {
-            Console.WriteLine($"WslRelay: listens on port {WSL_RELAY_PORT}");
+    //    void WslRelayStart()
+    //    {
+    //        Console.WriteLine($"WslRelay: listens on port {WaiterPort}");
 
-            using var listener = new TcpListener(IPAddress.Loopback, WSL_RELAY_PORT);
-            listener.Start();
+    //        using var listener = new TcpListener(IPAddress.Loopback, WaiterPort);
+    //        listener.Start();
 
-            try
-            {
-                while (true)
-                {
-                    var client = listener.AcceptTcpClient();
+    //        try
+    //        {
+    //            while (true)
+    //            {
+    //                var client = listener.AcceptTcpClient();
 
-                    using var networkStream = client.GetStream();
-                    Console.WriteLine("WslRelay: hello_world");
-                    var bytes = Encoding.UTF8.GetBytes("hello_world\n");
-                    networkStream.Write(bytes);
-                    networkStream.ReadExactly(bytes); //TODO (robin) why dont we get to this line?
-                    var incoming = Encoding.UTF8.GetString(bytes);
-                    Console.WriteLine("WslRelay: " + incoming);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("WslRelay: task faulted");
-                Console.WriteLine(ex);
-            }
+    //                using var networkStream = client.GetStream();
+    //                Console.WriteLine("WslRelay: hello_world");
+    //                var bytes = Encoding.UTF8.GetBytes("hello_world\n");
+    //                networkStream.Write(bytes);
+    //                networkStream.ReadExactly(bytes); //TODO (robin) why dont we get to this line?
+    //                var incoming = Encoding.UTF8.GetString(bytes);
+    //                Console.WriteLine("WslRelay: " + incoming);
+    //            }
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //            Console.WriteLine("WslRelay: task faulted");
+    //            Console.WriteLine(ex);
+    //        }
 
-            Console.WriteLine("WslRelay: task ended");
-        }
+    //        Console.WriteLine("WslRelay: task ended");
+    //    }
 
-        WslRealayThread = new Thread(new ThreadStart(WslRelayStart));
+    //    WslRealayThread = new Thread(new ThreadStart(WslRelayStart));
 
-        WslRealayThread.Name = "WslRelay Proc";
-        WslRealayThread.Start();
-    }
+    //    WslRealayThread.Name = "WslRelay Proc";
+    //    WslRealayThread.Start();
+    //}
 }
